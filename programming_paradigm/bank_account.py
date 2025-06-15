ee562e98fc9a322b907099981b26e52246373396
Nina <140155579+BeimnetTadesse@users.noreply.github.com>
2025-06-15 19:43:11 +0300

    Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,6 +12,5 @@ class BankAccount:
             return True
         return False
 
-def display_balance(self):
-    print(f"Current Balance: ${self.__account_balance:.2f}")
-
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance:.2f}")
